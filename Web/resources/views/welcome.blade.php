913258719b157010994534002eb622ea6304246b
bazingaterry <terry@32ph.com>
2016-10-05 10:07:30 +0800

    update demo

--- Web/resources/views/welcome.blade.php
+++ Web/resources/views/welcome.blade.php
@@ -1,91 +1,123 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+@section('content')
+    <div class="container">
+        <div class="row clearfix">
+            <div class="col-md-12 column">
+                <div class="jumbotron">
+                    <h1>{{ config('app.name', 'Laravel') }}</h1>
+                    <p>Miao, Moe, for SMIE.</p>
+                    <p>
+                        <a class="btn btn-primary btn-large" href="https://github.com/bazingaterry/OhMyCat">Star
+                            project</a></p>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        <div class="row clearfix">
+            <div class="col-md-12 column">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Topic</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="alert alert-dismissible alert-info">
+                            <button type="button" class="close" data-dismiss="alert">&times;</button>
+                            <h4>Warning!</h4>
+                            <p>Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra
+                                augue.
+                                Praesent commodo cursus magna,
+                                <a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p></div>
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <span class="badge">14</span>Cras justo odio<br>Dapibus ac facilisis in
+                            </li>
+                            <li class="list-group-item">
+                                <span class="badge">2</span>Dapibus ac facilisis in
+                            </li>
+                            <li class="list-group-item">
+                                <span class="badge">1</span>Morbi leo risus
+                            </li>
+                        </ul>
+                        <div class="btn-group">
+                            <a class="btn btn-primary btn-large" href="#">New Post</a>
+                            <a class="btn btn-default btn-large" href="#">Show All</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-md-12 column">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Big News</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="alert alert-dismissible alert-warning">
+                            <button type="button" class="close" data-dismiss="alert">&times;</button>
+                            <h4>Warning!</h4>
+                            <p>Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue.
+                                Praesent commodo cursus magna,
+                                <a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p></div>
+                        <table class="table table-striped table-hover ">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Column heading</th>
+                                <th>Column heading</th>
+                                <th>Column heading</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr class="info">
+                                <td>3</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr class="success">
+                                <td>4</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr class="danger">
+                                <td>5</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr class="warning">
+                                <td>6</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr class="active">
+                                <td>7</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
