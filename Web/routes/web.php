45996111f3a3a2edf317327cd1ef005bb60f816b
bazingaterry <terry@32ph.com>
2016-10-05 17:51:55 +0800

    add router

--- Web/routes/web.php
+++ Web/routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'HomeController@index');
+Route::resource('topic','TopicController');
 Auth::routes();
-
-Route::get('/home', 'HomeController@index');
