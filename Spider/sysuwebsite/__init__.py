eed33997423047f788b0724324a89efc1924493b
bazingaterry <terry@32ph.com>
2016-10-13 12:52:21 +0800

    exception handler

--- Spider/sysuwebsite/__init__.py
+++ Spider/sysuwebsite/__init__.py
@@ -42,14 +42,24 @@ class ElearningWebsite(Website):
         :return:
         """
         session = requests.Session()
-        cookies_site = session.get(
-            'http://elearning.sysu.edu.cn/webapps/blackboard/content/listContent.jsp?course_id=_12034_1&content_id=%s' % self.content_id).text
-        bs_obj = BeautifulSoup(cookies_site, 'html.parser')
-        redir_site = session.get(bs_obj.body.a['href']).text
-        bs_obj = BeautifulSoup(redir_site, 'html.parser')
-        bs_obj = BeautifulSoup(session.get(bs_obj.body.a['href']).text, 'html.parser')
-        ret = bs_obj.body.find_all(id='content_listContainer')
-        return str(ret).encode()
+        try:
+            cookies_site = session.get(
+                'http://elearning.sysu.edu.cn/webapps/blackboard/content/listContent.jsp?course_id=_12034_1&content_id=%s' % self.content_id)
+            if cookies_site.ok is not True:
+                return None
+            bs_obj = BeautifulSoup(cookies_site.text, 'html.parser')
+            redir_site = session.get(bs_obj.body.a['href'])
+            if redir_site.ok is not True:
+                return None
+            bs_obj = BeautifulSoup(redir_site.text, 'html.parser')
+            main_site = session.get(bs_obj.body.a['href'])
+            if main_site.ok is not True:
+                return None
+            bs_obj = BeautifulSoup(main_site.text, 'html.parser')
+            ret = bs_obj.body.find_all(id='content_listContainer')
+            return str(ret).encode()
+        except BaseException:
+            return None
 
 
 class XiaoxiWebsite(Website):
@@ -61,26 +71,36 @@ class XiaoxiWebsite(Website):
         小溪网抓取方法，解决 CSRF 后登陆，抓取文件列表。
         :return:
         """
+
         session = requests.Session()
-        login_site = session.get('http://172.18.187.11/netdisk/Logon.aspx').text
-        bs_obj = BeautifulSoup(login_site, 'html.parser')
-        form = bs_obj.body.form
-        input1 = form.select('#__VIEWSTATE')[0]
-        value1 = input1['value']
-        input2 = form.select('#__EVENTVALIDATION')[0]
-        value2 = input2['value']
-        session.post('http://172.18.187.11/netdisk/Logon.aspx', {
-            '__VIEWSTATE': value1,
-            '__EVENTVALIDATION': value2,
-            'txtVMName': '14web',
-            'txtUserName': 'anonymous',
-            'txtPassword': '123456',
-            'btnLogon': '登录'
-        })
-        r = session.get(
-            'http://172.18.187.11/netdisk/ShowFolderContent.aspx?vm=14web&foldername=%5c&rootindex=1&rootname=%e5%ae%9e%e9%aa%8c%e5%b8%83%e7%bd%ae')
-        bs_obj = BeautifulSoup(r.text, 'html.parser')
-        return str(bs_obj.body.table).encode()
+        try:
+            login_site = session.get('http://172.18.187.11/netdisk/Logon.aspx')
+            if login_site.ok is not True:
+                return None
+            bs_obj = BeautifulSoup(login_site.text, 'html.parser')
+            form = bs_obj.body.form
+            input1 = form.select('#__VIEWSTATE')[0]
+            value1 = input1['value']
+            input2 = form.select('#__EVENTVALIDATION')[0]
+            value2 = input2['value']
+            post = session.post('http://172.18.187.11/netdisk/Logon.aspx', {
+                '__VIEWSTATE': value1,
+                '__EVENTVALIDATION': value2,
+                'txtVMName': '14web',
+                'txtUserName': 'anonymous',
+                'txtPassword': '123456',
+                'btnLogon': '登录'
+            })
+            if post.ok is not True:
+                return None
+            r = session.get(
+                'http://172.18.187.11/netdisk/ShowFolderContent.aspx?vm=14web&foldername=%5c&rootindex=1&rootname=%e5%ae%9e%e9%aa%8c%e5%b8%83%e7%bd%ae')
+            if r.ok is not True:
+                return None
+            bs_obj = BeautifulSoup(r.text, 'html.parser')
+            return str(bs_obj.body.table).encode()
+        except BaseException:
+            return None
 
 
 class EdinWebsite(Website):
@@ -89,14 +109,20 @@ class EdinWebsite(Website):
 
     def read(self):
         s = requests.Session()
-        login_site = s.get('http://edin.sysu.edu.cn/wiki/doku.php?id=mad2016').text
-        bs = BeautifulSoup(login_site, 'html.parser')
-        a = bs.body.find_all(id='dokuwiki__content')[0]
-        return str(a).encode()
+        try:
+            login_site = s.get('http://edin.sysu.edu.cn/wiki/doku.php?id=mad2016')
+            if login_site.ok is not True:
+                return None
+            bs = BeautifulSoup(login_site.text, 'html.parser')
+            a = bs.body.find_all(id='dokuwiki__content')[0]
+            return str(a).encode()
+        except BaseException:
+            return None
 
 
 class FTPWebsite(Website):
-    def __init__(self, name, url, ip, port=21, username='anonymous', password='anonymous', encoding='utf-8', directory='/'):
+    def __init__(self, name, url, ip, port=21, username='anonymous', password='anonymous', encoding='utf-8',
+                 directory='/'):
         Website.__init__(self, name, url)
         self.ip = ip
         self.port = port
@@ -106,9 +132,12 @@ class FTPWebsite(Website):
         self.directory = directory
 
     def read(self):
-        ftp = FTP()
-        ftp.encoding = self.encoding
-        ftp.connect(self.ip, self.port)
-        ftp.login(self.username, self.password)
-        ftp.cwd(self.directory)
-        return ' '.join(ftp.nlst()).encode()
+        try:
+            ftp = FTP()
+            ftp.encoding = self.encoding
+            ftp.connect(self.ip, self.port)
+            ftp.login(self.username, self.password)
+            ftp.cwd(self.directory)
+            return ' '.join(ftp.nlst()).encode()
+        except BaseException:
+            return None
